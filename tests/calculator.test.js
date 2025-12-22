de1a4d0468ffc845ef2bd7f32674c6466bdefd7c
Deedy <natthapong.cha@ku.th>
2025-12-22 23:33:09 +0700

    edit -2 + -3 = -5

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -5,15 +5,15 @@ describe('Calculator', () => {
         expect(add(2, 3)).toBe(5);
     });
 
-    test('adds -2 + -3 to equal 5', () => {
-        expect(add(-2, -3)).toBe(5);
+    test('adds -2 + -3 to equal -5', () => {
+        expect(add(-2, -3)).toBe(-5);
     });
 
     test('subtracts 5 - 3 to equal 2', () => {
         expect(subtract(5, 3)).toBe(2);
     });
 
-    test('multiplies 4 * 3 to equal 12', () => {
+    test('multiplies 4 * 3 to equal 12', () => {    
         expect(multiply(4, 3)).toBe(12);
     });
 
